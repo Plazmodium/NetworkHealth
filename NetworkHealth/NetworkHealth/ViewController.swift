784610145c1cb16a29ce7e60d6796f195af15746
Plazmodium <gabzracz@icloud.com>
2018-03-12 21:18:34 +0200

    Check for network reachability - using RxSwift

--- NetworkHealth/NetworkHealth/ViewController.swift
+++ NetworkHealth/NetworkHealth/ViewController.swift
@@ -7,33 +7,53 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var networkGoodLbl: UILabel!
     @IBOutlet weak var networkBadLbl: UILabel!
+    @IBOutlet weak var refreshBtn: UIButton!
     
-
+    let dispose = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        checkNetwokStatus()
+        refreshBtn.rx.tap.bind{
+            
+            self.checkNetwokStatus().subscribeOn(MainScheduler.instance).subscribe({ (status) in
+                if(status.element == true){
+                    self.networkGoodLbl.isHidden = false
+                    self.networkBadLbl.isHidden = true
+                }else{
+                    self.networkGoodLbl.isHidden = true
+                    self.networkBadLbl.isHidden = false
+                }
+            }).disposed(by: self.dispose)
+            
+            }.disposed(by: dispose)
     }
-
+    
+    private func checkNetwokStatus() -> Observable<Bool>{
+        
+        return Observable.create({ (obs) -> Disposable in
+            
+            if ReachabilityStatus.isConnectedToNetwork(){
+                obs.onNext(true)
+                
+            }else{
+                obs.onNext(false)
+                obs.onCompleted()
+            }
+            return Disposables.create()
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    private func checkNetwokStatus(){
-        
-        if ReachabilityStatus.isConnectedToNetwork(){
-            networkGoodLbl.isHidden = false
-            networkBadLbl.isHidden = true
-        }else{
-            networkGoodLbl.isHidden = true
-            networkBadLbl.isHidden = false
-        }
     }
 }
 
